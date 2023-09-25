d51c9856e87bfc3178e222bc7a9748f11c36e5ff
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-09-26 01:22:58 +0300

    Update SectionTitle.js

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -1,6 +1,8 @@
 import { Section, Title } from "./SectionTitle.styled";
 
 export const SectionTitle = ({ title, children }) => {
+
+  console.log(children.props)
   return (
     <Section>
       <Title>{title}</Title>
